53420517447f132b404c888620878ad9f30d6342
Slocksan <977005@gmail.com>
2021-06-03 21:58:58 +0500

    Добавил управление

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
 
-    private Rigidbody2D rb;
+    public Rigidbody2D rb;
+    public Camera camera;
+
+    private Vector2 movementInput;
+    private Vector2 mousePositionInput;
+    
     // Start is called before the first frame update
     void Start()
     {
-        rb = new Rigidbody2D();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"),
+        movementInput = new Vector2(Input.GetAxis("Horizontal"),
             Input.GetAxis("Vertical"));
-        var mousePosition = Input.mousePosition;
-        transform.LookAt(rb., mousePosition);
+        mousePositionInput = camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movementInput * speed * Time.fixedDeltaTime);
+
+        var lookPoint = (mousePositionInput - rb.position);
+        var lookAngle = Mathf.Atan2(lookPoint.y, lookPoint.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = lookAngle;
     }
 }
