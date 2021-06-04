bb4c4247339d83050afd69a880434fcb1c8c388f
Slocksan <977005@gmail.com>
2021-06-04 13:56:49 +0500

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,11 +13,6 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 movementInput;
     private Vector2 mousePositionInput;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
     // Update is called once per frame
     void Update()
