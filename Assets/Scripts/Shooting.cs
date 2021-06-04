81afdd5b20eeefb4578d889e2b498f7c38ed3036
AlexJalapeno <stud0000231322@study.utmn.ru>
2021-06-04 20:45:03 +0500

    Стрельба

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    public float bulletSpeed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        GameObject tommyGunBullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = tommyGunBullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletSpeed, ForceMode2D.Impulse);
+    }
+}
