f1b92124f77ab9d252f0b3d2d32caa2ff3c5f1fd
Slocksan <977005@gmail.com>
2021-06-08 01:22:00 +0500

    Стрельба, бот и чет еще

---
+++ Assets/Scripts/Enemies/EnemyBehaviour.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public GameObject player;
+    public int HP;
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = player.transform.position - transform.position;
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+
+        rb.rotation = angle;
+        moveDirection = direction.normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + 
+                        moveDirection * speed * Time.fixedDeltaTime);
+    }
+}
--- Assets/Scripts/Shooting/BullerBehaviour.cs
+++ Assets/Scripts/Shooting/BullerBehaviour.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using Random = UnityEngine.Random;
 
 public class BullerBehaviour : MonoBehaviour
 {
@@ -10,7 +12,15 @@ public class BullerBehaviour : MonoBehaviour
     void Awake ()
     {
         SpriteRenderer bulletSprite = this.GetComponent<SpriteRenderer>();
-        bulletSprite.sprite = Sprites[Random.Range(0, 5)];
+        bulletSprite.sprite = Sprites[Random.Range(0, Sprites.Length)];
         Destroy (this.gameObject, 5f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Wall" && Random.Range(0, 5) != 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
