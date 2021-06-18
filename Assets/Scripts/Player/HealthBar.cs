992fec36c0ee49d72bae6081534bbf8795ac6a85
AlexJalapeno <stud0000231322@study.utmn.ru>
2021-06-18 17:13:54 +0500

    HP script

--- Assets/Scripts/Player/HealthBar.cs
+++ Assets/Scripts/Player/HealthBar.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Slider slider;
 
-    // Update is called once per frame
-    void Update()
+    public void SetMaxHealth(int health)
     {
-        
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    public void SetHealth(int health)
+    {
+        slider.value = health;
     }
 }
